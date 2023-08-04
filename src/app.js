2f464561f77149ae784a9d5c2c906d34915bd1ad
spdrzr182 <40244617+spdrzr182@users.noreply.github.com>
2023-08-04 16:34:49 -0700

    added hover effect

--- src/app.js
+++ src/app.js
@@ -5,16 +5,16 @@ grid.classList.add('grid');
 container.appendChild(grid)
 grid.addEventListener('click', (e) => {
     grid.classList.add('color')
-})
+});
 const clear = document.querySelector('#clear');
 clear.addEventListener ('click', erase); 
 function erase() {
     grid.classList.remove('color') 
     }; 
+grid.addEventListener ('mouseover', (e) => {
+    grid.classList.add('hover')
+});
+grid.addEventListener ('mouseout', (e) => {
+    grid.classList.remove('hover')
+});
 }
-
-
-
-//if (grid.classList.contains('color') === true) {
-    
-    //}
